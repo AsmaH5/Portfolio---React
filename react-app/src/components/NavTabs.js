0cd245719f1a5950a6f4acf5995093340a04a626
Asma Hassan <asmasheikhhassan@gmail.com>
2023-05-22 20:04:28 +0100

    Added a skills section and images

--- react-app/src/components/NavTabs.js
+++ react-app/src/components/NavTabs.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import "../styles/NavTabs.css"
+import "./styles/NavTabs.css"
 import MenuIcon from '@mui/icons-material/Menu';
 
 
--- react-app/src/components/pages/Home.js
+++ react-app/src/components/pages/Home.js
@@ -3,18 +3,33 @@ import "../styles/Home.css"
 
 function Home() {
   return (
-    <div className="HomeContainer">
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+    <div className="Home">
+      <div className="about">
+        <h2>Hi, My name is Asma Hassan </h2>
+        <div className="prompt">
+            <p>
+            I'm a Front-end Developer with a passion for designing and learning!
+            </p>
+            <button className="projectButton"/>
+            <button className="resume"/>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <img src="../assets/images/programmer" alt="programmer" />
+        <ul className="list">
+          <li className="item">HTML</li>
+          <li className="item">CSS</li>
+          <li className="item">JavaScript</li>
+          <li className="item">Git</li>
+          <li className="item">UX</li>
+          <li className="item">Bootstrap</li>
+          <li className="item">APIs</li>
+          <li className="item">JQuery</li>
+          <li className="item">JSON</li>
+          <li className="item">React</li>
+        </ul>
+      </div>
     </div>
   );
 }
