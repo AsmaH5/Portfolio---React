04b384b58db570aac7766b484d150009a589708a
Asma Hassan <asmasheikhhassan@gmail.com>
2023-05-23 01:55:28 +0100

    Edited insta and linkedin logo to link to the right sites

--- react-app/src/components/pages/Footer.js
+++ react-app/src/components/pages/Footer.js
@@ -9,11 +9,15 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
+      <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
         <InstagramIcon />
+        </a>
         <a href="https://github.com/AsmaH5" target="_blank" rel="noopener noreferrer">
         <GitHubIcon />
         </a>
+        <a href="https://www.linkedin.com/in/asma-h-757b14155/" target="_blank" rel="noopener noreferrer">
         <LinkedInIcon />
+        </a>
       </div>
       <p> &copy; 2023 asmahassan.com</p>
     </div>
